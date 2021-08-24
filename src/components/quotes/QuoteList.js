86675369190bbb04b63f3c07bcd75d72d02ac63e
PascaleStark <pascalestark.ca@gmail.com>
2021-08-24 12:03:13 -0400

    use query params to sort quotes in ascending or descending order --> useHistory, useLocation, new URLSearchParams

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,41 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from "react";
+import { useHistory, useLocation } from "react-router";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
+
+//helper function for sorting quotesList
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((A, B) => {
+    if (ascending) {
+      return A.id > B.id ? 1 : -1;
+    } else {
+      return A.id < B.id ? 1 : -1;
+    }
+  });
+};
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+  console.log(location);
+  const queryParams = new URLSearchParams(location.search);
+  const isSortingAscending = queryParams.get("sort") === "asc";
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+  const changeSortingHandler = () => {
+    //change the URL by adding query params using the History hook
+    history.push(`/quotes?sort=${isSortingAscending ? "desc" : "asc"}`);
+  };
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? "Descending" : "Ascending"}
+        </button>
+      </div>
+
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
--- src/pages/NewQuotes.js
+++ src/pages/NewQuotes.js
@@ -1,9 +1,12 @@
 import React from "react";
 import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from "react-router-dom";
 
 const NewQuote = () => {
+  const history = useHistory();
   const addQuoteHandler = (data) => {
     console.log(data);
+    history.push("/quotes");
   };
   return (
     <React.Fragment>

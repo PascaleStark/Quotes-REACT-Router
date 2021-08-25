5305ec03010ac67cb6209a6bc60e894a6807d819
PascaleStark <pascalestark.ca@gmail.com>
2021-08-25 14:31:05 -0400

    sending POST and GET requests to firebase

--- src/pages/NewQuotes.js
+++ src/pages/NewQuotes.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import QuoteForm from "../components/quotes/QuoteForm";
 import { useHistory } from "react-router-dom";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
+
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
   const addQuoteHandler = (data) => {
     console.log(data);
-    history.push("/quotes");
+    sendRequest(data);
   };
+
   return (
-    <React.Fragment>
-      <h1>Add a Quote</h1>
-      <QuoteForm onAddQuote={addQuoteHandler} />
-    </React.Fragment>
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler} />
   );
 };
 
